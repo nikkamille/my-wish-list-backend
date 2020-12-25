4c938410047a2d1b72e97e28fdc559a1f703b4d6
nikkamille <k.pagibigan@gmail.com>
2020-12-24 18:54:24 -0700

    create strong params

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -2,6 +2,13 @@ class WishListsController < ApplicationController
 
     def create
         byebug
+        wish_list = WishList.new(wish_list_params)
+    end
+
+    private
+
+    def wish_list_params
+        params.require(:wish_list).permit(:name)
     end
 
 end
