b8f572929b0f38f5ea4521687e04e4407fb9daa8
nikkamille <k.pagibigan@gmail.com>
2020-12-28 23:59:47 -0700

    add controller methods to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+
+    def index
+        render json: Item.all
+    end
+    
+    def create
+        item = Item.new(item_params)
+
+        if item.save
+            render json: item 
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price, :url, :image_url, :wish_list_id)
+    end
+
 end
