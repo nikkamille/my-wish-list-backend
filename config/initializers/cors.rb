764f69dfdcd083c40743636c52eb6bfdd8cb5bb2
nikkamille <k.pagibigan@gmail.com>
2021-01-02 00:12:44 -0700

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
+    
     resource '*',
       headers: :any,
+      expose: ["Authorization"]
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
