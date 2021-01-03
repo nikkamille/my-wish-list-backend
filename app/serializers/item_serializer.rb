ab559e92804f50b3bbe467aee5ede5b27fb0f657
nikkamille <k.pagibigan@gmail.com>
2021-01-02 20:54:07 -0700

    add id attribute to item serializer file

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :price, :url, :image_url
+  attributes :id, :name, :price, :url, :image_url
   belongs_to :wish_list
 end
